846b7e2fb9328ef365c67e85dfcbafda0750ec5e
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-10-15 12:15:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element II/majority-element-ii.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public: // code written by sunny
+    vector<int> findMajority(vector<int>& nums) {
+        int n = nums.size();
+    if (n == 0) return {-1};  
+    int candidate1 = 0, candidate2 = 0;
+    int count1 = 0, count2 = 0;
+    for (int num : nums) {
+        if (num == candidate1) {
+            count1++;
+        } else if (num == candidate2) {
+            count2++;
+        } else if (count1 == 0) {
+            candidate1 = num;
+            count1 = 1;
+        } else if (count2 == 0) {
+            candidate2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
+        }
+    }
+    count1 = 0;
+    count2 = 0;
+    for (int num : nums) {
+        if (num == candidate1) count1++;
+        else if (num == candidate2) count2++;
+    }
+    vector<int> result;
+    if (count1 > n / 3) result.push_back(candidate1);
+    if (count2 > n / 3) result.push_back(candidate2);
+    if (result.empty()) return {-1};
+    return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<int> ans = ob.findMajority(nums);
+        for (auto &i : ans)
+            cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
