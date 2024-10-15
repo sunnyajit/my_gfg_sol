37640c16cdfa31b9988679566fc15e04d92db01e
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-10-15 12:22:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+// User function template for C++
+
+class Solution {
+  public:
+  
+    vector<int> max_of_subarrays(int k, vector<int> &arr) {
+        // code written by sunny
+    deque<int> dq;
+    vector<int> result;
+
+    for (int i = 0; i < arr.size(); ++i) {
+        if (!dq.empty() && dq.front() == i - k) {
+            dq.pop_front();
+        }
+        while (!dq.empty() && arr[dq.back()] <= arr[i]) {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        if (i >= k - 1) {
+            result.push_back(arr[dq.front()]);
+        }
+    }
+
+    return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> res = obj.max_of_subarrays(k, arr);
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
