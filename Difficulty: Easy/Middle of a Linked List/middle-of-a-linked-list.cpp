9597cd4588b611ecc3cf83aac9aea9692ab516dc
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-10-15 12:32:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Initial template for C++
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/* Link list Nod
+/* Link list Node
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}; */
+class Solution {
+  public:
+    int getMiddle(Node* head) {
+        // code here
+         if (head == nullptr) {
+            return -1; // or any other indication of an empty list
+        }
+        
+        // Initialize two pointers
+        Node* slow = head;
+        Node* fast = head;
+        
+        // Traverse the list with two pointers
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        // When the loop ends, 'slow' points to the middle of the list
+        return slow->data;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after reading t
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
