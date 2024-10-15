eb27ed1df940f5e874fb8593d86c8047ceebb331
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-10-15 12:21:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int minimizeCost(int k, vector<int>& arr) {
+        // Code written by sunny
+         int n = arr.size();
+    vector<int> dp(n, numeric_limits<int>::max());
+    dp[0] = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j <= min(i + k, n - 1); ++j) {
+            dp[j] = min(dp[j], dp[i] + abs(arr[i] - arr[j]));
+        }
+    }
+
+    return dp[n - 1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.minimizeCost(k, arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
