7e9f0ddbaac00774de3ace302ed96dfa1ac7ede6
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-10-30 10:59:04 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with difference k/pairs-with-difference-k.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int countPairsWithDiffK(vector<int>& arr, int k) {
+        // code written by sunny
+        int p = 0;
+        unordered_map<int,int> mp;
+        for(int x:arr)mp[x]++;
+        for(int x:arr)p+=mp[x+k];
+        return p;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        auto ans = ob.countPairsWithDiffK(arr, k);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
