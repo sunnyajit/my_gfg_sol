cf7833b6a72abb9a9f726c494b7479311d308092
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-02 13:56:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth distance/kth-distance.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+
+    bool checkDuplicatesWithinK(vector<int>& arr, int k) {
+        // code written by sunny
+        unordered_set<int> seen;
+        for (int i = 0; i < arr.size(); ++i) {
+            if (seen.find(arr[i]) != seen.end()) {
+                return true;  
+            }
+            seen.insert(arr[i]);
+            if (i >= k) {
+                seen.erase(arr[i - k]);
+            }
+        }
+        return false; 
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        bool res = obj.checkDuplicatesWithinK(arr, k);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
