fd60ce7560422ad7c506b6308aef7fb5489894db
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-12-05 13:26:04 +0530

    Create Sort 0s, 1s and 2s.cpp

---
+++ Difficulty: Easy/Sort 0s, 1s and 2s.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public: // code written by sunny
+    void sort012(vector<int>& arr) {
+        int low = 0, mid = 0, high = arr.size() - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {  
+                swap(arr[mid], arr[high]);
+                high--;
+            }
+        }
+    }
+};
+
