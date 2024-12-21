e7fa77c997f53ce4b13fdd0cae0685dfe07a0649
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-12-21 21:18:48 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate by 90 degree/rotate-by-90-degree.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+
+class Solution {
+public: // code written by sunny
+    void rotateby90(vector<vector<int>>& mat) {
+        int n = mat.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            for (int i = 0, k = n - 1; i < k; i++, k--) {
+                swap(mat[i][j], mat[k][j]);
+            }
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(n, 0);
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.rotateby90(matrix);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j)
+                cout << matrix[i][j] << " ";
+            cout << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
