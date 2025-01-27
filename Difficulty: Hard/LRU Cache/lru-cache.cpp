2cd042c558ecd0d1355a22bd83c8f63b09070095
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2025-01-27 14:56:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/LRU Cache/lru-cache.cpp
@@ -0,0 +1,205 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// design the class in the most optimal way
+
+ 
+#include <bits/stdc++.h>
+using namespace std;
+// code written by sunny
+class LRUCache {
+private:
+    struct Node {
+        int key, value;
+        Node* prev;
+        Node* next;
+        Node(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+    };
+
+    int capacity;
+    unordered_map<int, Node*> cache;
+    Node* head;
+    Node* tail;
+
+    void addNodeToFront(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    void removeNode(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    Node* removeTail() {
+        Node* lru = tail->prev;
+        removeNode(lru);
+        return lru;
+    }
+
+public:
+    LRUCache(int cap) : capacity(cap) {
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            removeNode(node);
+            addNodeToFront(node);
+            return node->value;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            node->value = value;
+            removeNode(node);
+            addNodeToFront(node);
+        } else {
+            if (cache.size() == capacity) {
+                Node* lru = removeTail();
+                cache.erase(lru->key);
+                delete lru;
+            }
+            Node* newNode = new Node(key, value);
+            cache[key] = newNode;
+            addNodeToFront(newNode);
+        }
+    }
+
+    ~LRUCache() {
+        Node* current = head;
+        while (current) {
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+};
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int capacity;
+        cin >> capacity;
+        LRUCache *cache = new LRUCache(capacity);
+
+        int queries;
+        cin >> queries;
+        while (queries--) {
+            string q;
+            cin >> q;
+            if (q == "PUT") {
+                int key;
+                cin >> key;
+                int value;
+                cin >> value;
+                cache->put(key, value);
+            } else {
+                int key;
+                cin >> key;
+                cout << cache->get(key) << " ";
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
